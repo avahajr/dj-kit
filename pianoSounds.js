3f145f53497c1f1507ab12323a8bbc6edfcbb175
Ava Hajratwala <100488972+avahajr@users.noreply.github.com>
2023-12-03 19:59:25 -0500

    updated keymap

--- pianoSounds.js
+++ pianoSounds.js
@@ -1,17 +1,17 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-  var audioCtx: AudioContext;
-  var dynCompr: DynamicsCompressorNode;
-  const activeOscs = {};
-  const activeGains = {};
-  var recording = false;
-  const asdrTimes = {
-    attack: 0.1,
-    release: 0.1,
-  };
+var audioCtx;
+var dynCompr;
+const activeOscs = {};
+const activeGains = {};
+var recording = false; // to implement later
+const asdrTimes = {
+  attack: 0.1,
+  release: 0.1,
+};
 
+document.addEventListener("DOMContentLoaded", function (event) {
   const keyMap = {
     9: 261.625565300598634, // "TAB" c4
-    capslock: 293.66476791740756, // "CAPSLOCK" d4
+    20: 293.66476791740756, // "CAPSLOCK" d4
     65: 293.66476791740756, // "A" e4
     83: 329.627556912869929, // "S" f4
     68: 391.995435981749294, // "D" g4
@@ -21,22 +21,23 @@ document.addEventListener("DOMContentLoaded", function (event) {
     74: 587.33, // "J" d5
     75: 659.255, // "K" e5
     76: 698.456, // "L" f5
-    59: 783.991, // ";" g5
-    39: 880.0, // "'" a5
-    10: 987.767, // return ("\n") b5
-    92: 1046.5, // "\" c5
+    186: 783.991, // ";" g5
+    222: 880.0, // "'" a5
+    13: 987.767, // return ("\n") b5
+    220: 1046.5, // "\" c5
   };
 
-  function initAudio(): void {
-    audioCtx = new window.AudioContext();
+  function initAudio() {
+    audioCtx = new (window.AudioContext || window.webkitAudioContext)();
     dynCompr = audioCtx.createDynamicsCompressor();
     dynCompr.release.setValueAtTime(asdrTimes["release"], audioCtx.currentTime);
     dynCompr.attack.setValueAtTime(asdrTimes["attack"], audioCtx.currentTime);
   }
 
-  function playNote(key: string): void {
+  function playNote(key) {
     if (!audioCtx) {
       initAudio();
+      console.log("init audio");
     }
     const newOsc = audioCtx.createOscillator();
     newOsc.frequency.setValueAtTime(keyMap[key], audioCtx.currentTime);
@@ -50,21 +51,22 @@ document.addEventListener("DOMContentLoaded", function (event) {
     newOsc.connect(newGain).connect(dynCompr).connect(audioCtx.destination);
   }
 
-  function keyDown(e: KeyboardEvent): void {
-    const key = e.detail.toString();
+  function keyDown(e) {
+    console.log("keyDown");
+    const key = (e.detail || e.which).toString();
+    console.log(key);
     if (keyMap[key] && !activeOscs[key]) {
-      document.getElementById;
       playNote(key);
     }
+    console.log(audioCtx);
   }
 
-  function keyUp(e: KeyboardEvent): void {
-    const key = e.detail.toString();
+  function keyUp(e) {
+    const key = (e.detail || e.which).toString();
     if (keyMap[key] && activeOscs[key]) {
       activeOscs[key].stop();
     }
   }
-
   document.addEventListener("keydown", keyDown);
   document.addEventListener("keyup", keyUp);
 });
